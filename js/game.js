476c26cfae0b01884988215ad47dd8d1c0ee95ef
Ko Seoyoung <39763891+syoung125@users.noreply.github.com>
2020-01-06 18:31:56 +0100

    implement all function
    - Build box
    - Missile enemy

--- js/game.js
+++ js/game.js
@@ -10,6 +10,7 @@ var controls = {
 	Right: "ArrowRight",
 	Down: "ArrowDown",
 	Hide: "h",
+	Box: "b",
 	Space: " "
 }
 
@@ -41,15 +42,6 @@ function init(){
 	tileW = game.canvas.width/mapW;
 	tileH = (game.canvas.height - hBarH)/mapH;
 
-	// Make boxs
-	game.go.boxs = new Array();
-	for(var y = 0; y<mapH; y++){
-		for(var x=0; x<mapW; x++){
-			var tBox = new Box(x*tileW, hBarH + hBarB + y*tileH, tileW, tileH, gameMap1[(y*mapW)+x]);
-			game.go.boxs.push(tBox);
-		}
-	}
-
 	initGameObject();
 
 	// Add listner
@@ -63,7 +55,18 @@ function init(){
 			var tbullet = game.go.player.makeBullet();
 			game.go.bullet.push(tbullet);
 		}
+
+		if(!keysPressed[controls.Box] && game.go.player.buildReady){
+			var boxIndex = game.go.player.makeBox();
+			game.go.boxs[boxIndex].setType(1);
+		}
 	});	
+
+	// document.addEventListener("keypress", function (keyEvent) {
+	// 	if(keyEvent.key == 'b'){
+	// 		console.log('make box');
+	// 	}
+	// });
 
 
 	//start the loop
@@ -71,22 +74,33 @@ function init(){
 }
 
 function initGameObject(){
+	// Create map (x, y -> index)
+	game.go.boxs = new Array();
+	for(var y = 0; y<mapH; y++){
+		for(var x=0; x<mapW; x++){
+			var tBox = new Box(x*tileW, hBarH + hBarB + y*tileH, tileW, tileH, gameMap1[(y*mapW)+x]);
+			game.go.boxs.push(tBox);
+		}
+	}
+
 	// Create game object
-	game.go.player = new Player(120,150, 10, 10, "#ffff00", 300/1000, 300);
+	game.go.player = new Player(120,150, 10, 10, "#ffff00", 300/1000, 500);
 	
 	game.go.enemy = new Array();
 	// Fixed enemies
-	game.go.enemy.push(new Enemy(XcoorToPx(3), YcoorToPx(2), 100, "rgba(0,255,0,0.4)", 1, Math.PI*1/4, Math.PI*2/3));
-	game.go.enemy.push(new Enemy(XcoorToPx(1), YcoorToPx(4), 100, "rgba(0,255,0,0.4)", 1, Math.PI*1/4, Math.PI*1/6));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(3), IdxToCoor_Y(2), 100, "rgba(0,255,0,0.4)", 1, Math.PI*1/4, Math.PI*2/3));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(1), IdxToCoor_Y(4), 100, "rgba(0,255,0,0.4)", 1, Math.PI*1/4, Math.PI*1/6));
 	// Rotating enemies
-	game.go.enemy.push(new Enemy(XcoorToPx(8), YcoorToPx(6), 300, "rgba(0,0,255,0.5)", 4, Math.PI*1/3, Math.PI*3/2, 0.005));
-	game.go.enemy.push(new Enemy(XcoorToPx(3), YcoorToPx(8), 150, "rgba(0,0,255,0.3)", 2, Math.PI*1/3, Math.PI*7/5, 0.02, Math.PI*1/2));
-	game.go.enemy.push(new Enemy(XcoorToPx(8), YcoorToPx(9), 150, "rgba(0,0,255,0.3)", 2, Math.PI*1/3, Math.PI*5/6, 0.02, Math.PI*2/3));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(8), IdxToCoor_Y(6), 300, "rgba(0,0,255,0.5)", 4, Math.PI*1/3, Math.PI*3/2, 0.005, 0, false, 0, true));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(3), IdxToCoor_Y(8), 150, "rgba(0,0,255,0.3)", 2, Math.PI*1/3, Math.PI*7/4, 0.02, Math.PI*3/4));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(8), IdxToCoor_Y(9), 150, "rgba(0,0,255,0.3)", 2, Math.PI*1/3, Math.PI*5/4, 0.03, Math.PI*4/3, false, 0, true));
 	// Moving enemies
-	game.go.enemy.push(new Enemy(XcoorToPx(13)-tileH*0.3, YcoorToPx(7)-tileW*0.3, 100, "rgba(255,0,255,0.3)", 3, Math.PI*1/3, Math.PI*5/6, 0.02, Math.PI*1/3,
-	 true, -1, 0, 0.3));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(2)-tileH*0.3, IdxToCoor_Y(10)-tileW*0.3, 100, "rgba(255,0,255,0.3)", 3, Math.PI*1/4, Math.PI/4, 0.02, Math.PI*2/3, true, 0.8));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(13)-tileH*0.3, IdxToCoor_Y(7)-tileW*0.3, 100, "rgba(255,0,255,0.3)", 3, Math.PI*1/3, Math.PI, 0.03, Math.PI*2/3, true, 0.8));
+	game.go.enemy.push(new Enemy(IdxToCoor_X(15)-tileH*0.3, IdxToCoor_Y(2)-tileW*0.3, 100, "rgba(255,0,255,0.3)", 3, Math.PI*1/4, Math.PI/2, 0.03, Math.PI*1/2, true, 0.8));
 
 	game.go.bullet = new Array();
+	game.go.missile = new Array();
 
 	keysPressed = new Array();
 	game.lastTick = Date.now();
@@ -119,18 +133,23 @@ function update(delta){
 
 	// Check the collision between enemies and player.
 	for(var i = 0; i < game.go.enemy.length; i++){
-		if(!game.go.player.hide && checkCollision(game.go.enemy[i], game.go.player)){
+		if(!game.go.player.hide && check4Collision(game.go.enemy[i].path, game.go.player)){
 			game.go.enemy[i].isCollide(true);
 			game.go.player.dropHealth(game.go.enemy[i].strength);
+			if(game.go.enemy[i].ismissile && !game.go.enemy[i].isShooting){
+				var tmissile = game.go.enemy[i].setShootMissile(game.go.player);
+				game.go.missile.push(tmissile);
+			}
 		}else{
 			game.go.enemy[i].isCollide(false);
 		}
 
-		if(game.go.enemy[i].ismove && isHitWall(game.go.enemy[i].x, game.go.enemy[i].y)){
-			console.log("aaaaaaaaaa");
+		if(game.go.enemy[i].ismove && is4HitWall(game.go.enemy[i])){
+			// console.log("aaaaaaaaaa");
 			game.go.enemy[i].changeDirection();
 		}
 
+		// Kill enemy
 		if(game.go.enemy[i].life <= 0){
 			game.go.enemy.splice(i, 1);
 		}
@@ -138,13 +157,15 @@ function update(delta){
 		game.go.enemy[i].update(delta);
 	}
 
-	// Check the collision between wall(box) and player.
 	for(var i = 0; i < game.go.boxs.length; i++){
-		if(game.go.boxs[i].type == 1 && checkCollision(game.go.boxs[i], game.go.player)){
-			game.go.player.goToPre()
+
+		// Check the collision between wall(box) and player.
+		if(game.go.boxs[i].type == 1 && check4Collision(game.go.boxs[i].path, game.go.player)){
+			game.go.player.goToPre();
 		}
+			
 		// Win : when you reach to the goal
-		else if(game.go.boxs[i].type == 2 && checkCollision(game.go.boxs[i], game.go.player)){
+		if(game.go.boxs[i].type == 2 && check4Collision(game.go.boxs[i].path, game.go.player)){
 			 gameOver(1);
 			 return;
 		}
@@ -153,6 +174,7 @@ function update(delta){
 	// Update player
 	game.go.player.update(delta);
 	
+	// Update bullet
 	for(var i = 0; i<game.go.bullet.length; i++){
 		game.go.bullet[i].update();
 		// Get rid of the bullfet, When it hits the enemy or wall
@@ -161,8 +183,18 @@ function update(delta){
 		}
 	}
 
+	console.log(game.go.missile);
+	for(var i = 0; i<game.go.missile.length; i++){
+		game.go.missile[i].update(delta, game.go.player);
 
-	
+		// Get rid of the missile when hit the player or timeElapsedMissile
+		if(game.ctx.isPointInPath(game.go.missile[i].path, game.go.player.x, game.go.player.y)){
+			game.go.player.dropHealth(game.go.missile[i].strength);
+			game.go.missile.splice(i, 1);
+		}else if(game.go.missile[i].timeElapsedMissile >= 5000){
+			game.go.missile.splice(i, 1);
+		}
+	}
 
 
 	// Lose : When your health is over
@@ -199,6 +231,10 @@ function render(){
 		game.go.bullet[i].draw(game.ctx);
 	}
 
+	for(var i = 0; i<game.go.missile.length; i++){
+		game.go.missile[i].draw(game.ctx);
+	}
+
 
 }
 
@@ -211,7 +247,8 @@ function handleInput(delta) {
 
 	// Move player (Left, Up, Right, Down)
 	// ** Only when hide key dosen't be pressed
-	if(!game.go.player.hide){
+	if(!game.go.player.hide && !game.go.player.buildReady){
+
 		if(keysPressed[controls.Left]){
 			game.go.player.moveLeft(delta);
 		}
@@ -224,18 +261,39 @@ function handleInput(delta) {
 		if(keysPressed[controls.Down]){
 			game.go.player.moveDown(delta);
 		}
+
 	}
 
-	// Player press 'hide'
+	if(game.go.player.buildReady){
+		if(keysPressed[controls.Left]){
+			game.go.player.boxShapeUpdateXY(0);
+		}
+		else if(keysPressed[controls.Up]){
+			game.go.player.boxShapeUpdateXY(1);
+		}
+		else if(keysPressed[controls.Right]){
+			game.go.player.boxShapeUpdateXY(2);
+		}
+		else if(keysPressed[controls.Down]){
+			game.go.player.boxShapeUpdateXY(3);
+		}
+	}
+
+	// Player press 'h'
 	if(keysPressed[controls.Hide]){
-		game.go.player.hide = true;	
+		game.go.player.setHide(true);	
 	}else{
-		game.go.player.hide = false;
+		game.go.player.setHide(false);	
 	}
 
 	// Player press 'Space'
-	if(keysPressed[controls.Space] && !game.go.player.attackReady){
+	if(keysPressed[controls.Space]){
 		game.go.player.setAttackReady();
+	}
+
+	// Player press 'b'
+	if(keysPressed[controls.Box]){
+		game.go.player.setBuildReady();
 	}
 
 }
@@ -254,16 +312,28 @@ function drawMap() {
 /**
 * calculate index to coordinate
 */
-function XcoorToPx(i) {
+function IdxToCoor_X(i) {
 	var x = i*tileW;
 	return x;
 }
 
-function YcoorToPx(j) {
+function IdxToCoor_Y(j) {
 	var y = j*tileH + hBarH + hBarB;
 	return y;
 }
 
+/**
+* calculate coordinate to index
+*/
+function CoorToIdx_I(x){
+	var i = Math.floor(x/tileW);	//floor : round down
+	return i;
+}
+
+function CoorToIdx_J(y){
+	var j = Math.floor((y - hBarH - hBarB)/tileH);
+	return j;
+}
 
 /**
 * After game finish
@@ -289,13 +359,12 @@ function gameOver(isWin) {
 
 }
 
-function checkCollision(go, player) {
-	//check 4 points of player (4 edges)
-	if(game.ctx.isPointInPath(go.path, player.x, player.y) || 
-		game.ctx.isPointInPath(go.path, player.x + player.width, player.y) || 
-		game.ctx.isPointInPath(go.path, player.x, player.y + player.height) || 
-		game.ctx.isPointInPath(go.path, player.x + player.width, player.y + player.height)){
-		return true;
+function check4Collision(path, go) {
+	//check 4 points of player (4 edges which called colDot array)
+	for(var i = 0; i < go.colDot.length; i++){
+		if(game.ctx.isPointInPath(path, go.colDot[i].x, go.colDot[i].y)){
+			return true;
+		}
 	}
 	return false;
 }
@@ -308,6 +377,19 @@ function isHitWall(x, y){
 	for(var i = 0; i < game.go.boxs.length; i++){
 		if(game.go.boxs[i].type == 1 
 			&& game.ctx.isPointInPath(game.go.boxs[i].path, x, y)){
+			return true;
+		}
+	}
+	return false;
+}
+
+function is4HitWall(go){
+	var x, y, width, height;
+
+	width = height = 2*go.r;
+	for(var i = 0; i < game.go.boxs.length; i++){
+		if(game.go.boxs[i].type == 1 
+			&& check4Collision(game.go.boxs[i].path, go)){
 			return true;
 		}
 	}
